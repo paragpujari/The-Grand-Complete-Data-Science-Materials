b51610b08199f48b4b2ef2f2ce83d4fa60ceb86d
Parag Pujari <58661811+paragpujari@users.noreply.github.com>
2026-03-28 11:44:33 +0530

    Add files via upload

---
+++ Generative_AI_BootCamp/4-GenerativeAI/7-Machine_Learning/7.9-NaiveBayes/GaussianNaiveBayes.py
@@ -0,0 +1,43 @@
+import joblib
+import streamlit as st
+import numpy     as np
+import pandas    as pd
+
+#### Load the Model
+sc = joblib.load("StandardNB.pickle")
+gnb = joblib.load("GaussianNaiveBayes.pickle")
+
+
+#### Write the title of the Streamlit Application
+st.title("Gaussian Naive Bayes")
+
+#### Write the content into the Streamlit Application
+st.write("Predict whether the patient is having diabetes or not based on the diabetic condition")
+
+#### Enter the inputs
+pregnancies   = st.number_input("Pregnancies"             , min_value=0.0,step=0.5)
+glucose       = st.number_input("Glucose"                 , min_value=0.0,step=0.5)
+bloodpressure = st.number_input("BloodPressure"           , min_value=0.0,step=0.5)
+skinthickness = st.number_input("SkinThickness"           , min_value=0.0,step=0.5)
+insulin       = st.number_input("Insulin"                 , min_value=0.0,step=0.5)
+bmi           = st.number_input("BMI"                     , min_value=0.0,step=0.5)
+dpf           = st.number_input("DiabetesPedigreeFunction", min_value=0.0,step=0.5)
+age           = st.number_input("Age"                     , min_value=0.0,step=0.5)
+
+
+#### Predict whether the patient is sufering from diabetes or not
+if(st.button("Predict diabetes")):
+    #### convert the input into 2D array
+    input = np.array([[pregnancies, glucose, bloodpressure, skinthickness, insulin, bmi, dpf, age]])
+
+    #### scale the inputs
+    scaled_input = sc.transform(input)
+
+    #### Predict the model
+    predictions = gnb.predict(scaled_input)[0]
+    probability = gnb.predict_proba(scaled_input)[0][1]
+
+    if(predictions == 1):
+        st.error(f"High Risk of having Diabetes: (Probability : {probability:.2f})")
+    else:
+        st.success(f"Low Risk of having Diabetes: (Probability : {probability:.2f})")
